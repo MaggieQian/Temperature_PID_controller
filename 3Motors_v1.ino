67423dee19a90fc96176057ac79a8464fe7e3826
MaggieQian <98090953+MaggieQian@users.noreply.github.com>
2025-02-08 16:42:42 +0000

    Update 3Motors_v1.ino

--- 3Motors_v1.ino
+++ 3Motors_v1.ino
@@ -1,16 +1,17 @@
 #include <Servo.h>
 Servo myservo_1;
 Servo myservo_2;
+Servo myservo_3;
 int servoPin_1 = 9;
 int servoPin_2 = 10;
-int servoPin_2 = 11;
+int servoPin_3 = 11;
 int pos = 0;
 unsigned long time;
 
 void setup() {
   myservo_1.attach(servoPin_1); // set pin for servo control
   myservo_2.attach(servoPin_2);
-  myservo_2.attach(servoPin_3);
+  myservo_3.attach(servoPin_3);
 }
 
 void loop() {
